8ef85ef8e6c601e57c44a4d9d2a9a1659d9e8c64
Luis Martin Ghiglia <l.martinghiglia@globant.com>
2023-07-26 15:04:08 -0300

    feat(application): Add new layout
    BREAKING CHANGE: new features
    
    Changes to be committed:
    	modified:   src/Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Vinicius!");
+app.MapGet("/", () => "Hello Vinicius Vidal!");
 
 app.Run();
